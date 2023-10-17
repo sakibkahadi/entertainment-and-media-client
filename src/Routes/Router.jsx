d7dfc1a90d8c5a60211b016f53ec4778c19f8ec3
sakib kahadi <sakibkahadi72844@gamil.com>
2023-10-17 19:50:50 +0600

    basic css and route setup completed

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const myCreatedRoute = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+  
+export default myCreatedRoute
